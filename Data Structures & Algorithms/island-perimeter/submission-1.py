295eedc5a2858adccee7f6be5f55ba182bcc1759
Tommaso Nardi <narditommaso1@gmail.com>
2026-05-19 18:38:54 +0200

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows=len(grid)
+        cols=len(grid[0])
+
+        #Per ogni isola vediamo 
+        def ricorsione(x,y):
+            laticoperti=0
+            totale=0
+            grid[x][y] = 2
+            direzioni=[(x+1,y),(x,y+1),(x-1,y),(x,y-1)]
+            for dx,dy in direzioni:
+                if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] == 1:
+                    laticoperti+=1
+                    totale = totale+ricorsione(dx,dy)
+                elif 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] == 2:
+                    laticoperti+=1
+            return totale+(4-laticoperti)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]==1:
+                    return ricorsione(i,j)
