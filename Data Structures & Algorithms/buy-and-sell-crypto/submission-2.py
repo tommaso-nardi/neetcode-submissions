3ab940d62472df9e49007f84c6f3d3e8f1a01fbe
Tommaso Nardi <narditommaso1@gmail.com>
2026-05-20 20:49:45 +0200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        profitto=0
+        punt1=0
+        for punt2 in range(1,len(prices)):
+            if prices[punt1]>prices[punt2]:
+                punt1=punt2
+            if (prices[punt2]-prices[punt1]) > profitto:
+                profitto=prices[punt2]-prices[punt1]
+        return profitto
+            
