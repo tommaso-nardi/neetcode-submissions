5b4811eb85b51bad1c8abf941e652919c33de2bc
Tommaso Nardi <narditommaso1@gmail.com>
2026-05-23 11:32:12 +0200

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows=len(image)
+        cols=len(image[0])
+        og=image[sr][sc]
+
+        def ricorsione(x,y):
+            image[x][y]=color
+            direzioni=[(x+1,y),(x,y+1),(x-1,y),(x,y-1)]
+            for dx,dy in direzioni:
+                if 0 <= dx < rows and 0 <= dy < cols and image[dx][dy]==og:
+                    ricorsione(dx,dy)
+        
+        if image[sr][sc]==color:
+            return image
+        ricorsione(sr,sc)
+        return image
