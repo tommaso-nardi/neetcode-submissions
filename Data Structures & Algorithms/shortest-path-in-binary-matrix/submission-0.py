54ce1cf4b6879df5be8ae532d4bdd19f788ba195
Tommaso Nardi <narditommaso1@gmail.com>
2026-05-24 12:56:42 +0200

    Add: shortest-path-in-binary-matrix - submission-0

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0]!=0:
+            return -1
+        grid[0][0]=1
+        coda=deque([(0, 0)])
+        passi=1
+
+        while coda:
+            iterazioni=len(coda)
+            for i in range(iterazioni):
+                x,y=coda.popleft()
+                if x==len(grid)-1 and y==len(grid[0])-1:
+                    return passi
+                direzioni=[(x+1,y),(x,y+1),(x-1,y),(x,y-1),(x+1,y+1),(x-1,y+1),(x-1,y+1),(x+1,y-1)]
+                for dx,dy in direzioni:
+                    if 0 <= dx < len(grid) and 0 <= dy < len(grid[0]) and grid[dx][dy]==0:
+                        grid[dx][dy]=1
+                        coda.append((dx,dy))
+            passi+=1
+        
+        return -1
