0c6731f312c5db19b401c04348dc72bb69f1b9c0
Tommaso Nardi <narditommaso1@gmail.com>
2026-08-22 13:11:00 +0200

    Add: subarray-product-less-than-k - submission-1

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        punts=0
+        puntd=0
+        prodattuale=1
+        prodvalidi=0
+        #Logica che se c'è un subarray che va da X a Y che va bene allora
+        #tutti i subarray al suo interno vanno bene a loro volta
+        #l'aggiornamento prodvalidi+=((puntd-1)-punts+1) segue proprio questa regola
+        while puntd<len(nums):
+            prodattuale*=nums[puntd]
+            puntd+=1
+            while prodattuale>=k and punts<puntd:
+                prodattuale//=nums[punts]
+                punts+=1
+            prodvalidi+=((puntd-1)-punts+1)
+
+        return prodvalidi
